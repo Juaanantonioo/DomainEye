8fa4bf2439203ab9cd2869b39e17fa3ed1749625
Juan Calzada <calzadaj@hotmail.es>
2025-11-20 17:26:29 +0100

    [generar_recomendaciones_personalizadas] adaptado
    para generar recomendaciones especificas (del dominio mas peligroso), y de la media de dominios recibidos

--- generar_recomendaciones_personalizadas.py
+++ generar_recomendaciones_personalizadas.py
@@ -1,4 +1,6 @@
-import os
+from collections import Counter
+from datetime import datetime
+from typing import Protocol, List
 from google import genai
 from google.genai import types
 
@@ -19,6 +21,9 @@ def call_google_llm(prompt: str, model: str = "gemini-2.5-flash") -> str:
         )
     )
 
+    if response is None:
+        raise Exception("Error doing request to Google AI")
+
     return response.text
 
 def parse_list(s):
@@ -35,20 +40,72 @@ def parse_list(s):
 
 import ast
 
-def generar_recomendaciones(dominio_propio: str, dominio_externo: str,
-                            puntuacion_obtenida, puntuacion_maxima) -> list:
+def average(nums: List[int]) -> int:
+    if not nums:
+        raise ValueError("Cannot compute average of an empty list")
+    return sum(nums) // len(nums)
 
+def pais_frecuente(paises: List[str]) -> str:
+    if not paises:
+        raise ValueError("La lista está vacía")
+    contador = Counter(paises)
+    # `most_common(1)` devuelve una lista con tupla (pais, cantidad)
+    return contador.most_common(1)[0][0]
+
+class RecomendacionesParams:
+    # Datos generales de los dominios analizados
+    score_medio: float
+    creacion_media: datetime
+    pais_mas_frecuente: str
+
+
+    # Datos del dominio importante
+    domain: str
+    score: int
+    creation_date: datetime
+    country: str
+    action: str
+
+    # Datos generales
+    score_maximo: int
+    dominio_propio: str
+
+    def __init__(self, scores: list[int], creaciones: list[datetime], paises: list[str], importante_domain: str, importante_score: int, importante_creation_date: datetime, importante_country: str, importante_action: str, score_maximo: int, dominio_propio: str):
+        self.score_medio = average(scores)
+        creaciones_int = []
+        for creacion in creaciones:
+            creaciones_int.append(creacion.second)
+
+        seconds = average(creaciones_int)
+        self.creacion_media = datetime.utcfromtimestamp(seconds)
+
+        self.pais_mas_frecuente = pais_frecuente(paises)
+
+        self.domain = importante_domain
+        self.score = importante_score
+        self.creation_date = importante_creation_date
+        self.country = importante_country
+        self.action = importante_action
+
+        self.dominio_propio = dominio_propio
+        self.score_maximo = score_maximo
+
+
+
+def generar_recomendaciones(params: RecomendacionesParams) -> dict:
     num_frases = 5
 
     prompt = (
-        f"He obtenido una puntuacion de {puntuacion_obtenida} "
-        f"sobre {puntuacion_maxima} (mas es mayor seguridad) en una prueba "
-        f"de typesquatting para mi dominio {dominio_propio}, ya que el "
-        f"dominio {dominio_externo} podría suplantar la web de mi marca. "
-        f"Dame una lista de {num_frases} frases cortas para ayudar al "
-        f"generente a a solventar el problema, en concreto, teniendo el tipo de typesquatting y la gravedad asignada. Solo devuelve la lista, no digas nada mas, "
-        f"debe poder ser parseable por python ast, asi que no especifiques nada de markdown tampoco"
+        f"Se ha realizado un informe de typosqiatting."
+        f"Se han analizado varios dominios problemáticos. He obtenido una puntuacion de media de {params.score_medio} "
+        f"sobre {params.score_maximo} (mas es mayor seguridad) "
+        f"de typesquatting para mi dominio {params.dominio_propio}. El dominio mas problemático, con un score de {params.score}, del pais {params.country}, con dominio registrado el {params.creation_date} (hoy es {datetime.now()}) "
+        f"dominio {params.domain} podría suplantar la web de mi marca, el informe ha detectado que la action recomendada es {params.action}."
+        f"Dame una lista de {num_frases * 2} frases cortas para ayudar al "
+        f"generente a a solventar el problema, en concreto, teniendo en cuenta el tipo de typesquatting y la gravedad asignada. Solo devuelve la lista, no digas nada mas, "
+        f"debe poder ser parseable por python ast, asi que no especifiques nada de markdown tampoco. Las {num_frases} primeras frases deben estar relacionadas con las recomendaciones en general, las {num_frases} ultimas frases se relacionaran con el dominio mas problemático."
     )
+
 
 
     lista_str = ""
@@ -62,14 +119,18 @@ def generar_recomendaciones(dominio_propio: str, dominio_externo: str,
 
         lista_parsed = parse_list(lista_str)
 
-        if len(lista_parsed) == num_frases:
+        if len(lista_parsed) == num_frases * 2:
             break
         print("La salida del LLM fue invalida: ", lista_str)
-        
 
-    return lista_parsed
+
+    return {
+        "generales": lista_parsed[:num_frases],
+        "especificas": lista_parsed[num_frases:],
+    }
 
 
 if __name__ == "__main__":
-    res = generar_recomendaciones("ewala.es", "evvala.es", 20, 100)
+    params = RecomendacionesParams([12, 20, 32, 12, 15, 17], [datetime.now()], ["España", "España", "España", "Italia", "Italia", "Portugal"], "evvala.es", 30, datetime.now(), "España", "Alto riesgo: revisar contenido y considerar denuncia al registrador/hosting.", 100, "ewala.es")
+    res = generar_recomendaciones(params)
     print(res)

db2b16df97a9aa220e0a34206477626f357d4b17
Jorge <llanezarodriguezjorge@gmail.com>
2025-11-20 09:54:21 -0500

    añadida funcion para generar todos los dominios

--- genera_dominios.py
+++ genera_dominios.py
@@ -1,26 +1,14 @@
+import asyncio
+import aiodns
 import itertools
 import dnstwist
+import dnstwist_domain_generator as dns_dg
 
 COMMON_TLDS = [".com", ".net", ".org", ".io", ".shop", ".online"]
 
 def generate_registered_candidates(base_domain: str):
     name, _, tld = base_domain.partition(".")
     candidates = set()
-    
-    try: #dnstwist es herramienta OSINT estandar para generar homografos
-        fuzzed_domains = dnstwist.fuzz(base_domain)
-        for entry in fuzzed_domains:
-            domain_name = entry.get('domain-name')
-            punycode_name = entry.get('punycode')
-            
-            if domain_name:
-                candidates.add(domain_name)
-    
-            if punycode_name and punycode_name != domain_name:
-                candidates.add(punycode_name)
-
-    except Exception as e:
-        print(f"Advertencia: dnstwist falló para {base_domain}. Error: {e}")
 
     # 1. Cambios de TLD
     for new_tld in COMMON_TLDS:
@@ -31,19 +19,36 @@ def generate_registered_candidates(base_domain: str):
     for s in suffixes:
         candidates.add(name + s + "." + tld)
 
-    # 3. Reemplazos sencillos de caracteres
-    replaces = {'o': '0', 'e': '3', 'l': '1', 'a': '4'}
-    for i, c in enumerate(name):
-        if c in replaces:
-            candidates.add(name[:i] + replaces[c] + name[i+1:] + "." + tld)
-
     # Evitar el original
     candidates.discard(base_domain)
+    for candidate in candidates.copy():
+        if not asyncio.run(dns_dg.async_is_registered(aiodns.DNSResolver, candidate)):
+            candidates.discard(candidate)
 
-    import dnstwist_domain_generator as dns_dg
     for candidate in dns_dg.get_similar_registered_domains_sync_wrapper(base_domain):
         candidates.add(candidate)
 
     return sorted(candidates)
 
-print(generate_registered_candidates("ewala.es"))
+def generate_all_candidates(base_domain: str):
+    name, _, tld = base_domain.partition(".")
+    candidates = set()
+
+    # 1. Cambios de TLD
+    for new_tld in COMMON_TLDS:
+        candidates.add(name + new_tld)
+
+    # 2. Typos básicos (sufijos típicos de phishing)
+    suffixes = ["-login", "-secure", "-verify", "-support", "-update", "-payment"]
+    for s in suffixes:
+        candidates.add(name + s + "." + tld)
+
+    # Evitar el original
+    candidates.discard(base_domain)
+    
+    fuzzer = dnstwist.Fuzzer(base_domain)
+    fuzzer.generate()
+    for candidate in [e["domain"] for e in fuzzer.domains]:
+        candidates.add(candidate)
+
+    return sorted(candidates)
